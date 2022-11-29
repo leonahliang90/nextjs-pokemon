25bc27abe1923d285c38bb40ab6fbaf2887a82bd
Leon Tan <yuliong1015@gmail.com>
2022-11-29 18:09:41 +0800

    added middleware

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+
+  const requestHeaders = new Headers(request.headers)
+  requestHeaders.set('Leon-Request-Header', 'v13.0.0');
+
+  // You can also set request headers in NextResponse.rewrite
+  const response = NextResponse.next({
+    request: {
+      // New request headers
+      headers: requestHeaders,
+    },
+  })
+
+  // Set a new response header `x-hello-from-middleware2`
+  response.headers.set('Cloudflare-CDN-Cache-Control', 'public, s-maxage=10, stale-while-revalidate=10');
+  response.headers.set('CDN-Cache-Control', 'public, s-maxage=10, stale-while-revalidate=10');
+  response.headers.set('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=10');
+  return response;
+}
